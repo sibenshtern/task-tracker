b3f9d80d8671b7c74d4d4286d4e99a8f67021333
sibenshtern <sibenshtern@gmail.com>
2020-04-18 18:25:09 +0300

    create flask frame

--- ticktrack/__init__.py
+++ ticktrack/__init__.py
@@ -1,6 +1,11 @@
-from os import urandom
+import os
 
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = urandom(16)
+app.secret_key = os.urandom(16)
+
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run(port=port)
---
+++ ticktrack/app/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+
+blueprint = Blueprint('app', __name__, template_folder='templates')
